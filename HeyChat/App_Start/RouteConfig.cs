6a085a526880ac82975d19eaf85dc5a4923f20e0
alexle512 <alexle512@gmail.com>
2019-04-01 15:16:00 -0500

    first commit

---
+++ HeyChat/App_Start/RouteConfig.cs
@@ -0,0 +1,47 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace HeyChat
+{
+    public static class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Home",
+                url: "",
+                defaults: new { controller = "Home", action = "Index" }
+                );
+
+            routes.MapRoute(
+            name: "Login",
+            url: "login",
+            defaults: new { controller = "Home", action = "Index" }
+            );
+
+
+            routes.MapRoute(
+            name: "ChatRoom",
+            url: "chat",
+            defaults: new { controller = "Home", action = "Index" }
+        );
+
+
+           routes.MapRoute(
+           name: "UserTyping",
+           url: "cbat/typing",
+           defaults: new { controller = "Chat", action = "Typing" }
+           );
+
+
+        routes.MapRoute(
+        name: "SendMessage",
+        url: "chat/@{user}",
+        defaults: new { controller = "Chat", action = "SendMessage", user = "" }
+        );
+
+        }
+    }
+}
---
+++ HeyChat/Controllers/ChatController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PusherServer;
+
+namespace HeyChat.Controllers
+{
+    public class ChatController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (Session["user"] == null)
+            {
+                return Redirect("/");
+            }
+
+            ViewBag.currentUser = Session["user"];
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Typing()
+        {
+            string typer = Request.Form["typer"];
+            string socket_id = Request.Form["socket_id"];
+         
+
+            var options = new PusherOptions();
+            options.Cluster = "Pusher_APP_CLUSTER";
+
+            var pusher = new Pusher(
+              "749662",
+              "e04ac7f428b64a8d05fc",
+              "30a8ef6697dbb7c507b2",
+              options);
+
+            pusher.TriggerAsync(
+            "chat",
+            "typing",
+            new { typer = typer },
+            new TriggerOptions() { SocketId = socket_id });
+
+            return new HttpStatusCodeResult(200);
+        }
+
+        public JsonResult SendMessage( string user) {
+
+            return Json("");
+        }
+    }
+}
